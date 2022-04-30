30e15b6164f422445967ab4ded92be4d2f86cd79
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-30 19:59:54 +0430

    export component

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { default as Header } from './Header/Header';
+
+export { default as FirstSlide } from './FirstSlide/FirstSlide';
+export { default as HeadeSecondSlider } from './SecondSlide/SecondSlide';
+export { default as ThirdSlide } from './ThirdSlide/ThirdSlide';
+
+export { default as AuthHeader } from './AuthHeader/AuthHeader';
+
+export { default as InputField } from './InputField/InputField';
+export { default as EmailInput } from './InputField/EmailField/EmailField';
+export { default as PasswordInput } from './InputField/PasswordField/PasswordField';
+
+export { default as PrimaryButton } from './PrimaryButton/PrimaryButton';
