bb23996fb93c77467f138c73597c6758c87a240e
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-30 19:54:52 +0430

    added screen lovk portrait, splash screen

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { App } from './app';
+import { AuthProvider } from './context';
+import { SplashScreen } from '@capacitor/splash-screen';
+
+window.screen.orientation.lock('portrait');
+
+const splashScreen = async () => {
+  // Show the splash for two seconds and then automatically hide it:
+  await SplashScreen.show({
+    showDuration: 2000,
+    autoHide: true,
+  });
+};
+
+splashScreen();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
