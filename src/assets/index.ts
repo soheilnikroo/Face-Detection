504237045d705bea43d69e41e3407ca81258a94c
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-30 19:59:45 +0430

    export assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,8 @@
 export { default as faceDetection } from './svg/face-detection.svg';
-export { default as logo } from './image/circle-logo.png';
+export { default as logoDark } from './svg/logo-dark.svg';
+export { default as secondStep } from './svg/second-step.svg';
+export { default as logoWhite } from './svg/logo-light.svg';
+export { default as backgroundImage } from './svg/background-step-2.svg';
+export { default as thirdSlideImage } from './svg/third-slide-image.svg';
+export { default as LoginIamge } from './svg/login-image.svg';
+export { default as SiginImage } from './svg/sigin-image.svg';
