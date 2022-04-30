7607102138c6ed9565ff784f61360388d9b6a0a8
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-30 20:00:19 +0430

    export pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,5 @@
 export { default as HomePage } from './Home/Home';
+export { default as WelcomePage } from './Welcome/WelcomePage';
+
+export { default as LoginPage } from './Auth/Login/Login';
+export { default as SigninPage } from './Auth/Signin/Signin';
