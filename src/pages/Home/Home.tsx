e23318fe09e0282dd9f3af5e67eaca3231c9897f
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-30 19:58:59 +0430

    added coming soon text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
 import './style/Home.css';
 
 const Home: React.FC = () => {
@@ -17,7 +16,7 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <ExploreContainer />
+        <h1 style={{ fontSize: '2rem', color: '#000' }}>Coming Soon...</h1>
       </IonContent>
     </IonPage>
   );
